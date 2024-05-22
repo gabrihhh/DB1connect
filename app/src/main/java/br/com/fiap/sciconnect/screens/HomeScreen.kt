9140020a5c63a2fd06e9fe2c034297874faf5c1b
maycon-alves <maycon_valdemiro@hotmail.com>
2024-05-21 22:25:15 -0300

    implantação home + match

--- app/src/main/java/br/com/fiap/sciconnect/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/sciconnect/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package br.com.fiap.sciconnect.screens
 
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -19,6 +20,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -33,6 +38,17 @@ import br.com.fiap.sciconnect.R
 import br.com.fiap.sciconnect.components.Header
 import br.com.fiap.sciconnect.components.Navigation
 import br.com.fiap.sciconnect.model.User
+import br.com.fiap.sciconnect.service.RetrofitFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+var match by mutableStateOf<Boolean>(false)
+var Count by mutableStateOf<Int>(1)
+var ListaUsers by mutableStateOf(listOf<User?>(null))
+var nomePerfil by mutableStateOf("gabriel")
+var tituloPerfil by mutableStateOf("Desenvolvedor")
+var textoPerfil by mutableStateOf("Sou o seu guia na jornada da programação. Comigo, você vai explorar o fascinante mundo do JavaScript. Juntos, vamos mergulhar nos conceitos, desafios e possibilidades que essa linguagem oferece. Estou aqui para responder às suas perguntas, ajudar a resolver problemas e compartilhar dicas valiosas. Vamos codificar e transformar ideias em realidade!")
 
 @Composable
 fun HomeScreen(
@@ -60,6 +76,7 @@ fun HomeScreen(
                 .height(400.dp)
                 .border(BorderStroke(1.dp, Color.Black), shape = RoundedCornerShape(10.dp))
             ){
+              //Mexer aqui
                 Box(modifier = Modifier.fillMaxSize()){
                     Column(modifier = Modifier
                         .align(Alignment.BottomStart)
@@ -67,18 +84,24 @@ fun HomeScreen(
                         .height(200.dp)
                         .padding(10.dp),
                         verticalArrangement = Arrangement.SpaceAround
+
                     ){
                         Text(
-                            text = "Gabriel Patara",
+                            text = match.toString(),
                             fontWeight = FontWeight.Bold,
                             color = Color.Black
                         )
                         Text(
-                            text = "Mentor de Programação",
+                            text = nomePerfil,
+                            fontWeight = FontWeight.Bold,
                             color = Color.Black
                         )
                         Text(
-                            text = "Sou o seu guia na jornada da programação. Comigo, você vai explorar o fascinante mundo do JavaScript. Juntos, vamos mergulhar nos conceitos, desafios e possibilidades que essa linguagem oferece. Estou aqui para responder às suas perguntas, ajudar a resolver problemas e compartilhar dicas valiosas. Vamos codificar e transformar ideias em realidade!",
+                            text = tituloPerfil.toString(),
+                            color = Color.Black
+                        )
+                        Text(
+                            text = textoPerfil,
                             fontSize = 10.sp,
                             textAlign = TextAlign.Justify,
                             lineHeight = 15.sp,
@@ -96,7 +119,27 @@ fun HomeScreen(
                 horizontalArrangement = Arrangement.SpaceAround
             ){
                 Box(modifier = Modifier.clickable {
-                    //DESLIKE
+                    var call = RetrofitFactory()
+                        .getUsersService()
+                        .getUsuarios()
+                    call.enqueue(object : Callback<List<User>> {
+                        override fun onResponse(
+                            call: Call<List<User>>,
+                            response: Response<List<User>>
+                        ) {
+                            Log.i("FIAP", "onResponse: ${response.body()}")
+                            ListaUsers = response.body()!!
+                            nomePerfil = ListaUsers[Count]!!.nome
+                            tituloPerfil = ListaUsers[Count]!!.formadoTitulo
+                            textoPerfil = ListaUsers[Count]!!.texto
+                            match = false
+                            Count ++
+                        }
+                        override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                            Log.i("FIAP", "onResponse: ${t.message}")
+                        }
+
+                    })
                 }) {
                     Image(
                         painter = painterResource(id = R.drawable.notlike),
@@ -106,7 +149,34 @@ fun HomeScreen(
                 }
 
                 Box(modifier = Modifier.clickable {
-                    //LIKE
+                    var call = RetrofitFactory()
+                        .getUsersService()
+                        .getUsuarios()
+                    call.enqueue(object : Callback<List<User>> {
+                        override fun onResponse(
+                            call: Call<List<User>>,
+                            response: Response<List<User>>
+                        ) {
+                            Log.i("FIAP", "onResponse: ${response.body()}")
+                            ListaUsers = response.body()!!
+                            nomePerfil = ListaUsers[Count]!!.nome
+                            tituloPerfil = ListaUsers[Count]!!.formadoTitulo
+                            textoPerfil = ListaUsers[Count]!!.texto
+                            ListaUsers[Count]!!.interesses.forEach{ interessesPerfil ->
+                                if(user.value!!.interesses.contains(interessesPerfil)){
+                                    match = true
+                                }else{
+                                    match = false
+                                }
+                            }
+                            Count ++
+                        }
+
+                        override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                            Log.i("FIAP", "onResponse: ${t.message}")
+                        }
+
+                    })
                 }) {
                     Image(
                         painter = painterResource(id = R.drawable.like),
